010d9f7479ff5ebf63a73f4f8d39a3a80461cddd
dotSality <90727519+dotSality@users.noreply.github.com>
2022-02-17 14:12:41 +0300

    fixed mix-content error

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import s from "./App.module.scss";
 import loader from "./assets/img/loader.gif";
+import { useAppSelector } from "./bll/hooks";
 import { Collection } from "./features/Collection";
 import { Header } from "./features/Header";
-import { useAppSelector } from "./bll/hooks";
 
 const App = () => {
   const { isLoaded } = useAppSelector(state => state.movies);
